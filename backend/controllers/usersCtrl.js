ee4f95d80ff768fe9651e318678617e7a71a69fd
cosmonet-fr <stephane.billois@hotmail.com>
2020-08-12 14:15:54 +0200

    Raw SQL queries | Ok

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -13,13 +13,12 @@ exports.signup = (req, res, next) => {
       const user = User.create({
         email: req.body.email,
         username: req.body.username,
+        url_image: req.body.url_image,
+        bio: req.body.bio,
         //password: req.body.password
         password: hash
       });
-      //Save dans la DB ici
 
-    //.then(() => res.status(201).json({ message: 'Utilisateur crée' }))
-    //.catch(error => res.status(500).json({ message: 'Cette adresse mail semble être déjà utilisée' }));
     })
 };
 
@@ -36,23 +35,13 @@ exports.login = (req, res, next) => {
   }
 
   ////////////////////////::::
+  async function start() {
+    const sequelize = require('../connectDB');
+    const { QueryTypes } = require('sequelize');
+    const users = await sequelize.query("SELECT id, email, username, url_image, bio  FROM Users WHERE email='" + email + "' AND password='" + password + "'", { type: QueryTypes.SELECT });
+    console.table(users);
+    console.log('Vous êtes connecté en tant que',users[0].username);
 
-  const mariadb = require('mariadb');
-const db = mariadb.createConnection({
-host : 'localhost',
-user : 'bilbou',
-password : 'openclassrooms',
-database : 'groupomania_db_dev'
-});
-db.connect();
-
-  db.query('SELECT id FROM utilisateurs WHERE email='+email+' AND password='+password+'', (error, results, fields) => {
-if (error){
-throw error;
-}else{
-console.log('Résultat : '+result);
-}
-});
-db.end();
-
+  }
+  start();
 };
