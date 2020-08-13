ded4e1d1c872c674aff40fb45f2dd00015d3fa04
cosmonet-fr <stephane.billois@hotmail.com>
2020-08-13 15:27:52 +0200

    login ok | It remains to control the data.

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -28,20 +28,34 @@ exports.login = (req, res, next) => {
   console.table(req.body);
 
   //Récupération et validation des paramètres
-  let email = req.body.email;
-  let password = req.body.password;
-  if (email === undefined || password === undefined) {
-      res.status(400).json({ error: 'Il manque un paramètre' })
-  }
+  const email = req.body.email;
+
+
+
 
   ////////////////////////::::
-  async function start() {
-    const sequelize = require('../connectDB');
-    const { QueryTypes } = require('sequelize');
-    const users = await sequelize.query("SELECT id, email, username, url_image, bio  FROM Users WHERE email='" + email + "' AND password='" + password + "'", { type: QueryTypes.SELECT });
-    console.table(users);
-    console.log('Vous êtes connecté en tant que',users[0].username);
+  async function login() {
+    const login = await User.findOne({ where: { email: email } });
+if (login === null) {
+  return res.status(400).json({ errors: ["email ou mot de passe invalide"]  });
+
+} else {
+  bcrypt.compare(req.body.password, login.password, function(err, result) {
+      // result == true
+
+      if (result === false) {
+        return res.status(400).json({ errors: ["email ou mot de passe invalide"]  });
+
+      } else {
+        var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+        return res.status(200).json({ token: token });
+
+      }
+  });
+  console.log(login instanceof User); // true
+  console.log(login.email); // 'My Title'
+}
 
   }
-  start();
+  login();
 };
